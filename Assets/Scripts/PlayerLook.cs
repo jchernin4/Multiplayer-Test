294e3e51405e8966873da612540d141e0fc0839c
jchernin4 <jeremychernin@gmail.com>
2021-05-09 22:08:24 -0400

    Steamworks integrated+disabled

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -1,12 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
-using UnityEditor.Rendering;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
-using Mirror;
-using UnityEditor;
 
 public class PlayerLook : MonoBehaviour {
     public PlayerMovement movementScript;
--- Assets/Scripts/SteamLobby.cs
+++ Assets/Scripts/SteamLobby.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
+using Steamworks;
 using UnityEngine;
 
-public class SteamLobby : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class SteamLobby : MonoBehaviour {
+    public GameObject buttons = null;
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEntered;
+    private const string HOST_ADDRESS_KEY = "HostAddress";
+
+    private NetworkManager networkManager;
+
+    private void Start() {
+        networkManager = GetComponent<NetworkManager>();
+
+        if (!SteamManager.Initialized) {
+            return;
+        }
+
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void HostLobby() {
+        buttons.SetActive(false);
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, networkManager.maxConnections);
+    }
+
+    private void OnLobbyCreated(LobbyCreated_t callback) {
+        if (callback.m_eResult != EResult.k_EResultOK) {
+            buttons.SetActive(true);
+            return;
+        }
         
+        networkManager.StartHost();
+
+        SteamMatchmaking.SetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+            HOST_ADDRESS_KEY,
+            SteamUser.GetSteamID().ToString());
+    }
+
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback) {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback) {
+        if (NetworkServer.active) {
+            return;
+        }
+
+        string hostAddress = SteamMatchmaking.GetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+                HOST_ADDRESS_KEY);
+        networkManager.networkAddress = hostAddress;
+        networkManager.StartClient();
+        buttons.SetActive(false);
     }
 }
